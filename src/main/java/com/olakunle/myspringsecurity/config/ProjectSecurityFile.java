1bd95e4a7eedc761b770fe4ab862f5e53f604be6
olakunlebalogun <olakunlebalogun247@gmail.com>
2022-08-26 11:11:08 +0100

    starting a spring security project

---
+++ src/main/java/com/olakunle/myspringsecurity/config/ProjectSecurityFile.java
@@ -0,0 +1,27 @@
+package com.olakunle.myspringsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+//@EnableWebSecurity
+public class ProjectSecurityFile {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
---
+++ src/test/java/com/olakunle/myspringsecurity/MySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olakunle.myspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
